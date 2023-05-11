38c793496e199359141f47aad506ceb8e3423483
clakr <clarktolosa@gmail.com>
2023-05-11 10:40:12 +0800

    init project

---
+++ src/_main.ts
@@ -0,0 +1,14 @@
+function changeTextContent(element: HTMLTitleElement | Element | null){
+  if(!element) return
+
+  element.textContent = import.meta.env.VITE_PROJECT_NAME
+}
+
+(function () {
+
+  const title = document.querySelector("title");
+  changeTextContent(title)
+
+  const heading = document.querySelector('.sr-only')
+  changeTextContent(heading)
+})();
